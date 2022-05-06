29750c8f964fa171694fe5f161b9070a8105f9bc
Eve <82066881+evedl@users.noreply.github.com>
2022-05-05 23:56:38 -0500

    rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+//componentes
+import { CrearProductoComponent } from './components/crear-producto/crear-producto.component';
+import { ListarProductosComponent } from './components/listar-productos/listar-productos.component';
+
 const routes: Routes = [
-  {}
+  // rutas para redirigir la pagina 
+  { path: '', component: ListarProductosComponent },
+  { path: 'crear-producto', component: CrearProductoComponent },
+  // lleva el id del producto
+  { path: 'editar-producto/:id', component: CrearProductoComponent },
+  
+  //lugar a donde se van a renderizar estas rutas
+  { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
 
 @NgModule({
